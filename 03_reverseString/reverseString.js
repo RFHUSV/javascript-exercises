5639d428bbb482c68ab2a0c1b81b75b14517f9c6
rfhusv <rfhusv@gmail.com>
2023-01-30 15:16:25 +0400

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    let charArray = string.split("");
+    for (let i = (charArray.length - 1); i >= 0; i--) {
+        result += charArray[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
