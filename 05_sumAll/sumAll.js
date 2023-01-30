f26528d8ab527a2fec5f2979b39bfee29813619a
Rauf <rfhusv@gmail.com>
2023-01-30 20:23:33 +0400

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+    let sum = 0;
+    if (firstInt < 0 || lastInt < 0) {
+        return 'ERROR';
+    }
+    if ((typeof firstInt) !== "number" || (typeof lastInt) !== "number") {
+        return 'ERROR';
+    }
 
+    if (firstInt <= lastInt) {
+        for (let i = firstInt; i<= lastInt; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = firstInt; i>= lastInt; i--) {
+            sum += i;
+        }
+    }  
+    return sum;  
 };
 
 // Do not edit below this line
